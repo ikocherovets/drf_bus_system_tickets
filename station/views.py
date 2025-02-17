f2819417e6d524a6e5edc2cc8d96d73f8ccd7e70
illiakocherovets <illiakocherovets>
2025-02-18 00:57:54 +0100

    generic views

--- station/views.py
+++ station/views.py
@@ -5,36 +5,15 @@ from station.models import Bus
 from .serializers import BusSerializer
 
 class BusList(
-    generics.GenericAPIView,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin
+    generics.ListCreateAPIView,
 ):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
-
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs) -> Response:
-        return self.create(request, *args, **kwargs)
         
         
-
 class BusDetail(
-    generics.GenericAPIView,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin
+    generics.RetrieveUpdateDestroyAPIView,
 ):
     queryset =  Bus.objects.all()
     serializer_class = BusSerializer
-
-    def get(self, request, *args, **kwargs) -> Bus:
-        return self.retrieve(self, request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs) -> Response:
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs) -> Response:
-        return self.destroy(request, *args, **kwargs)
 
